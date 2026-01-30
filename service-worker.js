97ba71d74f989843ddcbd345b49a29950376b073
Nannys y Peques <nannysypeques@gmail.com>
2026-01-29 23:44:39 -0600

    272

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'nannys-pwa-v13'; // Increment version to v13
+const CACHE_NAME = 'nannys-pwa-v15'; // Increment version to v15
 const ASSETS_TO_CACHE = [
   '/',
   '/index.html',
